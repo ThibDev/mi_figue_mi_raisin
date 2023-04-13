5e3e7193a4a8f5ea70a72b7a6025887b99c2308b
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-13 13:26:23 +0200

    add ArticleForm, formUpdate, ReadArticle and readAllArticle

---
+++ views/Admin/Article/ArticleForm.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+
+    if (isset($erreurs)) {
+        var_dump($erreurs);
+    }
+
+    ?>
+
+    <body style="background-color: #e5e6e8;">
+
+        <form enctype="multipart/form-data" action="../../../controllers/Router.php?product=create" method="post" style="width: 50%;background-color: white; margin: 50px auto;border-radius: 15px;padding: 10px;">
+            <h1 style="font-family: sans-serif; text-align:center; padding-bottom : 15px">Ajout d'article'</h1>
+
+            <div class="form-outline">
+                <label class="form-label" for="form3Example1">Titre de l'article</label>
+                <input name="title" placeholder="Titre de l'article" type="text" id="form3Example1" class="form-control" />
+            </div>
+                
+            <div class="form-outline mb-4">
+                <label class="form-label" for="form3Example3">Texte</label>
+                <input name="text" placeholder="Texte" type="text" id="form3Example3" required class="form-control" />
+            </div>
+
+            <div class="form-outline mb-4">
+                <label class="form-label" for="form4">Photo</label>
+                <input name="picture" type="file" required id="form4" class="form-control" /> 
+            </div>
+
+            <div class="form-check d-flex justify-content-center mb-4">
+                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
+                <label class="form-check-label" for="form2Example33">
+                    J'ai lu et j'accepte les politiques de confidentialés de ce site
+                </label>
+            </div>
+
+            <div class="submit" style="display: flex;">
+                <button type="submit" class="btn btn-primary btn-block mb-4" style="width: 50%; margin:auto;">Envoyer</button>
+            </div>
+
+
+
+        </form>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+
+</html>
---
+++ views/Admin/Article/ReadAllArticle.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <table class="table table-dark">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Titre</th>
+                <th scope="col">Texte</th>
+                <th scope="col">Photo</th>
+                <th scope="col">Auteur</th>
+                <th scope="col">Créer le</th>
+                <th scope="col">Ajout</th>
+                <th scope="col">Modifier</th>
+                <th scope="col">Supprimer</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($articles as $article) : ?>
+                <tr>
+                    <th style="background-color: whitesmoke; color: black" scope="row"><?= $article["id_article"]; ?></th>
+                    <td style="background-color: whitesmoke; color: black"><?= $article["title"]; ?></td>
+                    <td style="background-color: whitesmoke; color: black"><?= $article["text"]; ?></td>
+                    <td style="background-color: whitesmoke; color: black"><?= $article["picture"]; ?></td>
+                    <td style="background-color: whitesmoke; color: black"><?= $article["id_user"]; ?></td>
+                    <td style="background-color: whitesmoke; color: black"><?= $article["created_at"]; ?></td>
+                    <td style="background-color: whitesmoke; color: black"><a href="../views/Admin/Article/ArticleForm.php?article=create"><i class="fa-solid fa-plus"></i></a></td>
+                    <td style="background-color: whitesmoke; color: black"><a href="../controllers/Router.php?article=formUpdate&id_article=<?= $article["id_article"] ?>"><i class="fa-solid fa-pen"></a></i></a></td>
+                    <td style="background-color: whitesmoke; color: black"><a href="../controllers/Router.php?article=delete&id_article=<?= $article["id_article"] ?>"><i class="fa-solid fa-trash-can"></i></a></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    </div>
+    </div>
+
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
